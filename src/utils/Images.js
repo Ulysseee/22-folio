1a3db57fb2c53164fe0d29f0819679bdd96edcb6
Ulysseee <62332779+Ulysseee@users.noreply.github.com>
2022-05-30 19:27:32 +0200

    [wip] rick ashley and some clean

--- src/utils/Images.js
+++ src/utils/Images.js
@@ -2,7 +2,7 @@ import spectrum from '/img/spectrum.png'
 import orsay from '/img/orsay.png'
 import metamorph from '/img/metamorph.png'
 import mesh from '/img/mesh.png'
-import image05 from '/img/6.jpg'
+import rickRoll from '/img/rick-roll.gif'
 
 import cinema from '/img/cinema.gif'
 import music from '/img/music.gif'
@@ -10,7 +10,7 @@ import photo from '/img/photo.gif'
 import museum from '/img/museum.gif'
 import games from '/img/games.gif'
 
-const works = [spectrum, orsay, metamorph, mesh, image05]
+const works = [spectrum, orsay, metamorph, mesh, rickRoll]
 const passions = [cinema, music, photo, museum, games]
 
 export { works, passions }
